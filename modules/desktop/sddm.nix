fb893d16bbad980429f53723932d399309620598
Fletcher Kidwell <fletcher.kidwell@gmail.com>
2026-09-10 16:19:24 -0500

    fprintd fixed and cleaned

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -3,7 +3,5 @@
 {
   services.displayManager.sddm.enable = true;
 
-  security.polkit.enable = true;
-  services.fprintd.enable = true;
-  security.pam.services.hyprlock = {};
+  
 }
--- modules/programs/laptop.nix
+++ modules/programs/laptop.nix
@@ -25,4 +25,26 @@
   };
 
   services.logind.settings.Login.HandlePowerKey = "suspend";
+
+
+  security.polkit.enable = true;
+  services.fprintd.enable = true;
+  security.pam.services.hyprlock = {};
+
+  systemd.services.fprintd-resume = {
+    description = "Restart fprintd after suspend";
+
+    wantedBy = [
+      "suspend.target"
+    ];
+
+    after = [
+      "suspend.target"
+    ];
+
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/sleep 2; ${pkgs.systemd}/bin/systemctl restart fprintd.service'";
+    };
+  };
 }
